cded20900894bd2804f17bdd94f2fa02ffd038b0
asrtem-krisanov <kordbit69@icloud.com>
2020-09-23 13:34:32 +0300

    Random Quote Machine

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import QuoteAndAuthor from './QuoteAndAuthor';
+import quotes from './QuoteDB';
+
+import './App.css';
+
+export default class App extends Component {
+
+  //state
+  state = {
+    quote: quotes[0].quote,
+    author: quotes[0].author
+  }
+
+  //generate diffrent quote function
+  generateRandomQuote = (arr) => {
+    //get random numbers
+    let num = Math.floor(Math.random() * quotes.length)
+
+    let newQuote = quotes[num];
+
+    //update state
+    this.setState({
+      quote: newQuote.quote,
+      author: newQuote.author
+    })
+
+    this.shuffleQuotes(quotes)
+
+  }
+
+  //shuufle quotes function
+  shuffleQuotes = (arr) => {
+    return arr.sort(function () { return 0.5 - Math.random() });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h1 className="text-center">Random Quote</h1>
+        <QuoteAndAuthor
+          generateRandomQuote={this.generateRandomQuote}
+          quote={this.state}
+        />
+      </div>
+    )
+  }
+}
